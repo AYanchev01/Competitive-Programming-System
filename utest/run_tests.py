21f2936a337e78d1db653e32dd55e345a57fd172
Anton Yanchev <ayanchev01@gmail.com>
2023-02-12 02:19:22 +0200

    utests for compiler

--- utest/run_tests.py
+++ utest/run_tests.py
@@ -2,9 +2,51 @@ import sys
 import os
 sys.path.append(os.getcwd())
 from services import system
+from services.compiler import c_compile,cpp_compile
 import unittest
-import pathlib
+from pathlib import Path
 import shutil
+
+class TestCompiler(unittest.TestCase):
+    def setUp(self):
+        self.test_folder = Path("test_folder")
+        self.test_folder.mkdir(exist_ok=True)
+
+    def tearDown(self):
+        for file in self.test_folder.glob("*"):
+            file.unlink()
+        self.test_folder.rmdir()
+
+    def test_c_compile(self):
+        c_file = self.test_folder / "Test.c"
+        c_file.write_text("#include <stdio.h>\n int main() { return 0; }")
+
+        exe_file = c_compile(c_file)
+
+        self.assertTrue(Path(exe_file).exists())
+
+    def test_c_compile_error(self):
+        c_file = self.test_folder / "Test.cpp"
+        c_file.write_text("#include <stdio.h>\n int main() { return 0; }")
+
+        with self.assertRaises(ValueError):
+            c_compile(c_file)
+
+    def test_cpp_compile(self):
+        cpp_file = self.test_folder / "Test.cpp"
+        cpp_file.write_text("#include <iostream>\n int main() { return 0; }")
+
+        exe_file = cpp_compile(cpp_file)
+
+        self.assertTrue(Path(exe_file).exists())
+
+    def test_cpp_compile_error(self):
+        cpp_file = self.test_folder / "Test.c"
+        cpp_file.write_text("#include <iostream>\n int main() { return 0; }")
+
+        with self.assertRaises(ValueError):
+            cpp_compile(cpp_file)
+
 
 class TestSystem(unittest.TestCase):
     def setUp(self):
@@ -13,7 +55,7 @@ class TestSystem(unittest.TestCase):
 
     def test_system_python(self):
         shutil.copy2("utest/sysadmin.py", "../program_files/sysadmin.py")
-        program_file = pathlib.Path("../program_files/sysadmin.py")
+        program_file = Path("../program_files/sysadmin.py")
         problem = "Sysadmin"
         language = "Python"
         output, score = system.system(language, program_file, problem)
@@ -21,7 +63,7 @@ class TestSystem(unittest.TestCase):
 
     def test_system_c(self):
         shutil.copy2("utest/sysadmin.c", "../program_files/sysadmin.c")
-        program_file = pathlib.Path("../program_files/sysadmin.c")
+        program_file = Path("../program_files/sysadmin.c")
         problem = "Sysadmin"
         language = "C"
         output, score = system.system(language, program_file, problem)
@@ -29,7 +71,7 @@ class TestSystem(unittest.TestCase):
 
     def test_system_cpp(self):
         shutil.copy2("utest/sysadmin.cpp", "../program_files/sysadmin.cpp")
-        program_file = pathlib.Path("../program_files/sysadmin.cpp")
+        program_file = Path("../program_files/sysadmin.cpp")
         problem = "Sysadmin"
         language = "C++"
         output, score = system.system(language, program_file, problem)
